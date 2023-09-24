649c17f7067cc81b48568f249766b03f4be529c2
tragdate <tragdate@gmail.com>
2023-09-24 03:41:32 +0300

    add zsh completion

--- completion.bash
+++ completion.bash
@@ -1,28 +1,21 @@
-_biblia-cli_comp() {
-    COMPREPLY=( $(compgen -W "$1" -- ${word}) )
-    if [[ ${#COMPREPLY[@]} == 1 && ${COMPREPLY[0]} == "--"*"=" ]]; then
-        # If there's only one option, with =, then discard space
-        complete -o nospace
-    fi
-}
+if [ -n "$ZSH_VERSION" ]; then
+    autoload -U +X bashcompinit && bashcompinit
+fi
 
 _biblia-cli() {
-    COMPREPLY=()
-    complete +o default # Disable default to not deny completion, see: http://stackoverflow.com/a/19062943/1216348
+local word=${COMP_WORDS[COMP_CWORD]}
+local prev=${COMP_WORDS[COMP_CWORD-1]}
 
-    local word="${COMP_WORDS[COMP_CWORD]}"
-    local prev="${COMP_WORDS[COMP_CWORD-1]}"
-
-    case "${COMP_CWORD}" in
-        1)
-            if [[ $word == -* ]]; then
-                _biblia-cli_comp '-l -W -h'
-            else
-                biblia-cli -l | while read b;
-                    do echo ${b% *}
-                done
-            fi
-            ;;
-    esac
+case "${COMP_CWORD}" in
+    1)
+        if [[ $word == -* ]]; then
+            COMPREPLY=( $(compgen -W '-l -W -h' -- ${word}) )
+        else
+            COMPREPLY=( $(biblia-cli -l | awk '{print $1}') )
+        fi
+        ;;
+esac
 }
+
 complete -F _biblia-cli biblia-cli
+
